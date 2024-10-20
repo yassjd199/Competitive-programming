5cb7eb05f5e5fc4afbfffc1f4cc1634262b64d59
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-10-20 23:24:03 +0100

    Create Nearest_Smaller_Values.cpp

---
+++ STL/Nearest_Smaller_Values.cpp
@@ -0,0 +1,96 @@
+// @author : yassjd199
+//https://cses.fi/problemset/task/1645/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+double eps = std::numeric_limits<double>::epsilon();
+#define f first
+#define s second
+#define pb push_back
+#define endl '\n'
+#define ll  long long int
+#define ull unsigned long long int
+const int dx[4] = {0, 0, -1, 1}, dy[4] = { -1, 1, 0, 0};
+//const int dx[8] = {0, 0, -1, 1, -1, -1, 1, 1} , dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
+const long long mod = 1e9 + 7;
+const int N = 1e6 + 1, M = 20;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using Map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void dbg_out() {
+	cerr << endl;
+}
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+	cerr << ' ' << H;
+	dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+
+
+
+
+
+
+void testcase() {
+	int n;
+	cin >> n;
+	vii v(n);
+	for (auto &x : v) cin >> x;
+	vii ans(n, 0);
+	stack<int>st ;
+	for (int i = n - 1; i >= 0; i--) {
+		if (st.empty() || v[i] >= v[st.top()]) {
+			st.push(i);
+			//dbg("ok");
+		}
+		else {
+			while (!st.empty() && v[st.top()] > v[i]) {
+				ans[st.top()] = i + 1;
+				st.pop();
+			}
+			st.push(i);
+		}
+	}
+	for (auto x : ans) cout << x << " ";
+
+
+
+
+
+
+}
+
+int main() {
+	ios::sync_with_stdio (0);
+	cin.tie (0);
+	cout.tie (0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int _ = 1;
+	//int __ = 1;
+	clock_t z = clock();
+	//cin >> _;
+
+	while (_--) {
+		//cout<<"Case #"<<__++<<": ";
+		testcase();
+	}
+
+	cerr << endl << "Run Time: " << (( double ) ( clock() - z ) / CLOCKS_PER_SEC ) << endl;
+	return 0;
+}
+
+/*   /\_/\
+*   (= ._.)
+*   / >  \>
+*/
