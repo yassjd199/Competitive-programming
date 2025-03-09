dd26384a92f0d3ec7b653b2e373e189524f6379b
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-03-09 14:44:52 -0600

    Create Maximum width.cpp

---
+++ Greedy/Maximum width.cpp
@@ -0,0 +1,98 @@
+/**
+ * Created on: 2025-03-09
+ * Author: yassjd199
+ * 
+ * https://codeforces.com/contest/1492/problem/C
+ * 
+ * match from left to right , then in reverse , the answer is the max of one of them , this is the idea not the solution !
+ */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int , int> pii;
+typedef pair<long long , long long> pll;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+double eps = std::numeric_limits<double>::epsilon( );
+#define f first
+#define s second
+#define pb push_back
+#define endl '\n'
+#define ll long long int
+#define ull unsigned long long int
+const int dx[4] = { 0, 0, -1, 1 } , dy[4] = { -1, 1, 0, 0 };
+// const int dx[8] = {0, 0, -1, 1, -1, -1, 1, 1}, dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
+const long long mod = 1e9 + 7;
+const int N = 1e6 + 1 , M = 20;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template<typename T> using Set = tree<T , null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update>;
+template<typename K , typename V> using Map = tree<K , V , less<K> , rb_tree_tag , tree_order_statistics_node_update>;
+
+void dbg_out( ) {
+    cerr << endl;
+}
+template <typename Head , typename... Tail>
+void dbg_out(Head H , Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+
+
+
+void testcase( ) {
+    int n , m; cin >> n >> m;
+    string S , T; cin >> S >> T;
+    int ans = 0;
+    vii first(m) , last(m);
+    for (int i = 0 , j = 0; j < m; i++) {
+        if (S[i] == T[j]) {
+            first[j++] = i;
+        }
+    }
+    for (int i = n - 1 , j = m - 1; j >= 0; i--) {
+        if (S[i] == T[j]) {
+            last[j--] = i;
+        }
+    }
+
+    for (int i = 1; i < m; i++) {
+        ans = max(ans , last[i] - first[i - 1]);
+    }
+    cout << ans << endl;
+
+
+
+
+
+}
+
+int main( ) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int _ = 1;
+    // int __ = 1;
+    clock_t z = clock( );
+    //cin >> _;
+
+    while (_--) {
+        // cout << "Case #" << __++ << ": ";
+        testcase( );
+    }
+
+    cerr << endl << "Run Time: " << ((double)(clock( ) - z) / CLOCKS_PER_SEC) << endl;
+    return 0;
+}
+
+/*   /\_/\
+*   (= ._.)
+*   / >  \>
+*/
