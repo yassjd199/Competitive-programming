b304ef7ab1ae859820639ef6d6b71211d2a82586
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-08-22 23:13:39 +0100

    Create To 3.cpp

---
+++ Dynamic Programming/To 3.cpp
@@ -0,0 +1,93 @@
+/**
+ * Created on: 2025-08-22
+ * Author: yassjd199
+ * https://atcoder.jp/contests/abc182/tasks/abc182_c
+ */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int , int> pii;
+typedef pair<long long , long long> pll;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+double eps = std::numeric_limits<double>::epsilon( );
+#define f first
+#define s second
+#define pb push_back
+#define endl '\n'
+#define ll long long int
+#define ull unsigned long long int
+const int dx[4] = { 0, 0, -1, 1 } , dy[4] = { -1, 1, 0, 0 };
+// const int dx[8] = {0, 0, -1, 1, -1, -1, 1, 1}, dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
+const long long mod = 1e9 + 7;
+const int N = 1e6 + 1 , M = 20;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template<typename T> using Set = tree<T , null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update>;
+template<typename K , typename V> using Map = tree<K , V , less<K> , rb_tree_tag , tree_order_statistics_node_update>;
+
+void dbg_out( ) {
+    cerr << endl;
+}
+template <typename Head , typename... Tail>
+void dbg_out(Head H , Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+
+void testcase( ) {
+    ll x; cin >> x;
+    string str = to_string(x);
+    int n = str.size( );
+    vector<vii> dp(n + 1 , vii(3));
+    dp[0][1] = dp[0][2] = 1000000000;
+    dp[0][0] = n;
+    for (int i = 1; i <= n; i++) {
+        for (int r = 0; r < 3; r++) {
+            dp[i][r] = dp[i - 1][r];
+            int x = str[i - 1] - '0';
+            x %= 3;
+            int r1 = r - x + 3;
+            r1 %= 3;
+            dp[i][r] = min(dp[i][r] , dp[i - 1][r1] - 1);
+        }
+    }
+
+    cout << (dp[n][0] < n ? dp[n][0] : -1);
+
+
+
+
+
+}
+
+
+int main( ) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int _ = 1;
+    // int __ = 1;
+    clock_t z = clock( );
+    // cin >> _;
+
+    while (_--) {
+        // cout << "Case #" << __++ << ": ";
+        testcase( );
+    }
+
+    cerr << endl << "Run Time: " << ((double)(clock( ) - z) / CLOCKS_PER_SEC) << endl;
+    return 0;
+}
+
+/*   /\_/\
+*   (= ._.)
+*   / >  \>
+*/
