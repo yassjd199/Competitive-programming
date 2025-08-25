5d4e5b734ca5fe6f248ce0612d6416c506cec6a0
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-08-26 00:11:49 +0100

    Create MST1.cpp

---
+++ Dynamic Programming/MST1.cpp
@@ -0,0 +1,88 @@
+/**
+ * Author: yassjd199
+ * https://www.spoj.com/problems/MST1/
+ *
+ */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int , int> pii;
+typedef pair<long long , long long> pll;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+double eps = std::numeric_limits<double>::epsilon( );
+#define f first
+#define s second
+#define pb push_back
+#define endl '\n'
+#define ll long long int
+#define ull unsigned long long int
+const int dx[4] = { 0, 0, -1, 1 } , dy[4] = { -1, 1, 0, 0 };
+// const int dx[8] = {0, 0, -1, 1, -1, -1, 1, 1}, dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
+const long long mod = 1e9 + 7;
+const int N = 1e6 + 1 , M = 20;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template<typename T> using Set = tree<T , null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update>;
+template<typename K , typename V> using Map = tree<K , V , less<K> , rb_tree_tag , tree_order_statistics_node_update>;
+
+void dbg_out( ) {
+    cerr << endl;
+}
+template <typename Head , typename... Tail>
+void dbg_out(Head H , Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+
+const int MX = 20000001;
+
+int dp[MX + 1];
+
+
+void testcase( ) {
+    int n ;
+    cin >> n;
+    cout << dp[n] << endl;
+
+}
+
+
+
+int main( ) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int _ = 1;
+    int __ = 1;
+    clock_t z = clock( );
+    cin >> _;
+
+    fill (dp, dp + MX + 1, 1e9);
+    dp[1] = 0;
+    for (int i = 2; i <= MX; i++) {
+        dp[i] = 1 + dp[i - 1];
+        if (i % 2 == 0) dp[i] = min(dp[i], 1 + dp[i / 2]);
+        if (i % 3 == 0) dp[i] = min(dp[i] , 1 + dp[i / 3]);
+    }
+
+    while (_--) {
+        cout << "Case " << __++ << ": ";
+        testcase( );
+    }
+
+    cerr << endl << "Run Time: " << ((double)(clock( ) - z) / CLOCKS_PER_SEC) << endl;
+    return 0;
+}
+
+/*   /\_/\
+*   (= ._.)
+*   / >  \>
+*/
