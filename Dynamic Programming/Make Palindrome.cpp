bb93e1409221b443243f6fdd6bfc0d17e62200d9
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-10-04 16:06:55 +0100

    Create  Make Palindrome.cpp

---
+++ Dynamic Programming/Make Palindrome.cpp
@@ -0,0 +1,93 @@
+
+// https://vjudge.net/problem/UVA-10453
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+double eps = std::numeric_limits<double>::epsilon();
+#define f first
+#define s second
+#define pb push_back
+#define endl '\n'
+#define ll  long long int
+#define ull unsigned long long int
+const int dx[4] = {0, 0, -1, 1}, dy[4] = { -1, 1, 0, 0};
+//const int dx[8] = {0, 0, -1, 1, -1, -1, 1, 1} , dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
+const long long mod = 1e9 + 7;
+const int N = 1e6 + 1, M = 20;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using Map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+string txt ;
+int n;
+int dp[1002][1002];
+
+int solve(int l = 0, int r = n - 1) {
+    if (l > r) return 0;
+    int &ret = dp[l][r];
+    if (~ret) return ret ;
+    ret = 1e9;
+    if (txt[l] == txt[r]) ret = min(ret, solve(l + 1, r - 1));
+    else {
+        ret = min({ret, 1 + solve(l + 1, r), 1 + solve(l, r - 1)});
+    }
+    return ret;
+}
+void print(int l = 0, int r = n - 1) {
+    if (l > r) return ;
+    int &ret = dp[l][r];
+    if (txt[l] == txt[r]) {
+        cout << txt[l];
+        print(l + 1, r - 1);
+        if (l != r) cout << txt[r];
+    } else {
+        if (ret == 1 + solve(l + 1, r) ) {
+            cout << txt[l];
+            print(l + 1, r);
+            cout << txt[l];
+        } else {
+            cout << txt[r];
+            print(l, r - 1);
+            cout << txt[r];
+        }
+    }
+
+}
+
+void testcase() {
+    while (cin >> txt) {
+        n = txt.size();
+        memset(dp, -1, sizeof(dp));
+        cout << solve() << " ";
+        print();
+        cout << endl;
+    }
+
+}
+
+int main() {
+    // ios::sync_with_stdio (0);
+    // cin.tie (0);
+    // cout.tie (0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int _ = 1;
+    int __ = 1;
+    clock_t z = clock();
+    //cin >> _;
+
+    while (_--) {
+        //cout << "Case " << __++ << ": ";
+        testcase();
+    }
+
+    cerr << endl << "Run Time: " << (( double ) ( clock() - z ) / CLOCKS_PER_SEC ) << endl;
+    return 0;
+}
