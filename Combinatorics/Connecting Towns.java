e5b2e747c0cfe90514d74608617d476beebbc828
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-10-29 19:40:38 +0100

    Create Connecting Towns.java

---
+++ Combinatorics/Connecting Towns.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+/*
+ Author: yassjd199
+ https://www.hackerrank.com/challenges/connecting-towns/problem
+    
+
+*/
+
+class Result {
+
+    /*
+     * Complete the 'connectingTowns' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY routes
+     */
+
+
+    static int MOD = 1234567;
+    public static int connectingTowns(int n, List<Integer> routes) {
+    // Write your code here
+        int ans =1;
+        for( Integer x : routes){
+            x%=MOD ;
+            ans*=x;
+            ans%=MOD;
+        }
+        
+        return ans  ;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> routes = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                int result = Result.connectingTowns(n, routes);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
