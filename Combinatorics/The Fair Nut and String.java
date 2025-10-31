aa94d7b27df7d4412a7b33d66662ce70ba75b45b
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-10-31 21:24:00 +0100

    Create The Fair Nut and String.java

---
+++ Combinatorics/The Fair Nut and String.java
@@ -0,0 +1,133 @@
+
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/1084/problem/C
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void testcase() {
+        String S = in.next();
+
+        StringBuilder T = new StringBuilder();
+
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == 'a' || S.charAt(i) == 'b') {
+                T.append(S.charAt(i));
+            }
+        }
+
+        int n = T.length();
+
+        long ans = 1;
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (T.charAt(i) == 'a')
+                cnt++;
+            else {
+                cnt++;
+                cnt %= MOD;
+                ans *= cnt;
+                ans %= MOD;
+                cnt = 0;
+            }
+        }
+        cnt++;
+        cnt %= MOD;
+        ans *= cnt;
+        ans %= MOD;
+        ans = ans - 1 + MOD;
+        ans %= MOD;
+        out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        // t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
