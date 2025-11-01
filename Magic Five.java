19cd0de286938d0bbdb8d9c5e13ea919a8d2b43d
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-01 10:51:52 +0100

    Create Magic Five.java

---
+++ Magic Five.java
@@ -0,0 +1,144 @@
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/327/problem/C
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static long exp(long a, long b) {
+        // a %= MOD;
+        long ret = 1L;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                ret *= a;
+                ret %= MOD;
+            }
+            a *= a;
+            a %= MOD;
+            b >>= 1;
+        }
+        return ret;
+    }
+
+    static long inv(long a) {
+        return exp(a, MOD - 2);
+    }
+
+    static long C(int n, int k) {
+        return (F[n] * inv((F[k] * F[n - k]) % MOD)) % MOD;
+
+    }
+
+    static long F[] = new long[N];
+
+    static void testcase() {
+        char S[] = in.next().toCharArray();
+        long k = in.nextLong();
+        long l = S.length;
+        long ans = 0;
+        for (int i = 0; i < l; i++) {
+            if (S[i] == '0' || S[i] == '5') {
+                long d = i;
+                ans += ((exp(2, d) * ((exp(2, (l * k)) - 1 + MOD) % MOD) % MOD)
+                        * (inv((exp(2, l) - 1 + MOD) % MOD)));
+                ans %= MOD;
+            }
+        }
+        out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        // t = in.nextInt();
+        // F[0] = F[1] = 1L;
+        // for (int i = 2; i <= 100000; i++) {
+        // F[i] = (i * 1L % MOD) * (F[i - 1]);
+        // F[i] %= MOD;
+        // }
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
