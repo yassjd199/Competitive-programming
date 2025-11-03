0b1b6dc0e7710d8a9e247699e518cc44cd1c7514
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-03 01:05:13 +0100

    Create onstructive Problem.java

---
+++ Other/onstructive Problem.java
@@ -0,0 +1,163 @@
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/1820/problem/C
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void testcase() {
+        int n = in.nextInt();
+        int A[] = in.readArray(n);
+
+        HashSet<Integer> ts = new HashSet<>();
+        for (int x : A) {
+            ts.add(x);
+        }
+
+        boolean ok = (ts.size() < n);
+        int mex = 0;
+        for (;; mex++) {
+            if (!ts.contains(mex)) {
+                break;
+            }
+        }
+        if (mex == 0) {
+            out.println("YES");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (A[i] > mex + 1) {
+                ok = true;
+            }
+        }
+        if (ts.contains(mex + 1)) {
+            int l = 0;
+            for (int i = 0; i < n; i++) {
+                if (A[i] == mex + 1) {
+                    l = i;
+                    break;
+                }
+            }
+            int r = n - 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (A[i] == mex + 1) {
+                    r = i;
+                    break;
+                }
+            }
+            int mex1 = 0;
+            ts = new HashSet<>();
+            for (int i = 0; i < n; i++) {
+                if (i >= l && i <= r)
+                    continue;
+                ts.add(A[i]);
+            }
+            if (ts.isEmpty()) {
+                out.println("NO");
+                return;
+            }
+            for (;; mex1++) {
+                if (!ts.contains(mex1))
+                    break;
+
+            }
+            // dbg(mex1);
+            out.println(mex1 == mex ? "YES" : "NO");
+
+        } else {
+            out.println(ok ? "YES" : "NO");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
