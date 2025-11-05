e87536d0f19a21c4336e9fc8c84eaee880a9be61
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-05 20:05:58 +0100

    Create Dora and Search.java

---
+++ Two_pointers/Dora and Search.java
@@ -0,0 +1,132 @@
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/1793/problem/C
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void testcase() {
+        int n = in.nextInt();
+        int A[] = in.readArray(n);
+
+        int l = 0, r = n - 1;
+        int L = 1, R = n;
+        while (l < r) {
+            if (A[l] == L || A[l] == R) {
+                if (A[l] == L)
+                    L++;
+                else
+                    R--;
+                l++;
+                continue;
+            }
+            if (A[r] == L || A[r] == R) {
+                if (A[r] == L)
+                    L++;
+                else
+                    R--;
+                r--;
+                continue;
+
+            }
+            if (A[l] != L && A[l] != R && A[r] != L && A[r] != R) {
+                // dbg(L, R, l, r);
+                out.printf("%d %d\n", l + 1, r + 1);
+                return;
+            }
+
+        }
+        out.println(-1);
+
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
