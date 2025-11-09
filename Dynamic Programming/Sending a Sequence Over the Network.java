a53d86744ad30826705c188c8dc4c10dfff64e08
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-09 23:38:30 +0100

    Create Sending a Sequence Over the Network.java

---
+++ Dynamic Programming/Sending a Sequence Over the Network.java
@@ -0,0 +1,123 @@
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/1741/problem/E
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+    static int n, B[];
+
+    static void testcase() {
+        n = in.nextInt();
+        B = in.readArray(n);
+
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        int ok[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            // option 1:
+            if (i - B[i - 1] > 0) {
+                dp[i] |= dp[i - B[i - 1] - 1];
+
+            }
+            // option 2:
+            dp[i] |= ok[i];
+
+            // update ok: (u can update dp directly no need for ok anyway )
+            if (i + B[i - 1] <= n)
+                ok[i + B[i - 1]] |= dp[i - 1];
+        }
+
+        out.println(dp[n] == 1 ? "YES" : "NO");
+
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
