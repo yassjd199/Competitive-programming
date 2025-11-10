cc12a0a5395cadb606763f09b8ee1d1d88bc4fa8
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-10 11:05:23 +0100

    Create Two Arrays.java

---
+++ Dynamic Programming/Two Arrays.java
@@ -0,0 +1,118 @@
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/1288/problem/C
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void testcase() {
+        int n, m;
+        n = in.nextInt();
+        m = in.nextInt();
+        long dp[][] = new long[(m << 1) + 1][n + 1];
+        Arrays.fill(dp[0], 1);
+        long p[][] = new long[(m << 1) + 1][n + 1];
+        for (int i = 1; i <= n; i++)
+            p[0][i] = p[0][i - 1] + dp[0][i];
+        for (int i = 1; i <= (m << 1); i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] += p[i - 1][j];
+                dp[i][j] %= MOD;
+                p[i][j] = p[i][j - 1] + dp[i][j];
+                p[i][j] %= MOD;
+            }
+        }
+        out.println(dp[m << 1][n]);
+
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        // t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
