2ff52c0cdb01cc6493d3d3054fee89581651db6d
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-12 14:23:28 +0100

    Create Element Extermination.java

---
+++ Other/Element Extermination.java
@@ -0,0 +1,124 @@
+/*
+ * Author: yassjd199
+ * https://codeforces.com/contest/1375/problem/C
+ */
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void testcase() {
+        int n = in.nextInt();
+        int A[] = in.readArray(n);
+        int L[] = new int[n];
+        Arrays.fill(L, -1);
+
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && (A[i] < A[st.peek()])) {
+                L[st.peek()] = i;
+                st.pop();
+            }
+            st.add(i);
+
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (A[i] > A[n - 1]) {
+                if (L[i] == -1) {
+                    out.println("NO");
+                    return;
+                }
+            }
+        }
+        out.println("YES");
+
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
