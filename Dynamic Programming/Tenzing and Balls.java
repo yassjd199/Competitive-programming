f99174186e1c2e0282912efb431139073022fdb7
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2025-11-13 22:19:20 +0100

    Update Tenzing and Balls.java

--- Dynamic Programming/Tenzing and Balls.java
+++ Dynamic Programming/Tenzing and Balls.java
@@ -1,25 +1,64 @@
 /*
  * Author: yassjd199
- * https://codeforces.com/contest/1842/problem/C
- * Look at the recurrence, try to transform it, simplify the complexity
+ * https://codeforces.com/problemset/problem/1842/C
+ * Look at the recurrence try group the indicies in a simple way !
  */
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.Math.abs;
-import static java.lang.System.out;
+import static java.lang.Math.floor;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.round;
 
-
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
 
-    // ----------- Fast Input -----------
+    static final int N = 1000006;
+    static final int MOD = 1000000000 + 7;
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+    static final double EPS = 1e-9;
+    static FastReader in = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void testcase() {
+        int n = in.nextInt();
+        int A[] = in.readArray(n);
+
+        int best[] = new int[n + 1];
+        int dp[] = new int[n + 1];
+
+        Arrays.fill(best, -10000000);
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = max(dp[i - 1], best[A[i - 1]] + i);
+            best[A[i - 1]] = max(best[A[i - 1]], dp[i - 1] - i + 1);
+
+        }
+        out.println(dp[n]);
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int t = 1;
+        t = in.nextInt();
+        for (int tt = 1; tt <= t; tt++) {
+            testcase();
+        }
+        out.flush();
+        out.close();
+        long end = System.currentTimeMillis();
+        System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
+    }
+
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
@@ -51,52 +90,63 @@ public class Main {
             return Double.parseDouble(next());
         }
 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
         String nextLine() {
-            String str = "";
             try {
-                str = br.readLine();
+                return br.readLine();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return str;
+            return "";
         }
     }
 
-    // --------------------------------
+    /*
+     * /\_/\
+     * (= ._.)
+     * / > \>
+     */
+}
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.round;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
     static final int N = 1000006;
     static final int MOD = 1000000000 + 7;
-    //    static final int[] dx = {0, 0, -1, 1};
-    //    static final int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1, -1, -1, 1, 1};
-    static int[] dy = {-1, 1, 0, 0, -1, 1, -1, 1};
-
+    static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+    static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
     static final double EPS = 1e-9;
-    static int[] a = new int[N];
     static FastReader in = new FastReader();
     static PrintWriter out = new PrintWriter(System.out);
 
-    static void dbg(Object... o) {
-        System.err.println(Arrays.deepToString(o));
-    }
-
-    //-------------------------------------
-    static int n;
-    static int[] dp = new int[N];
-    static int[] best = new int[N];
-//    static int[] bestIndx = new int[N];
-
-
     static void testcase() {
-        n = in.nextInt();
+        int n = in.nextInt();
+        int A[] = in.readArray(n);
+
+        int best[] = new int[n + 1];
+        int dp[] = new int[n + 1];
+
+        Arrays.fill(best, -10000000);
+
         for (int i = 1; i <= n; i++) {
-            a[i] = in.nextInt();
-            dp[i] = 0;
-            best[a[i]] = -1000000000;
-        }
-        for (int i = 1; i <= n; i++) {
-            dp[i] = dp[i - 1];
-            dp[i] = max(dp[i], best[a[i]] + i);
-            best[a[i]] = max(best[a[i]], dp[i - 1] - (i - 1));
+            dp[i] = max(dp[i - 1], best[A[i - 1]] + i);
+            best[A[i - 1]] = max(best[A[i - 1]], dp[i - 1] - i + 1);
+
         }
         out.println(dp[n]);
     }
@@ -106,7 +156,6 @@ public class Main {
         int t = 1;
         t = in.nextInt();
         for (int tt = 1; tt <= t; tt++) {
-//            out.print("Case #" + tt + ": ");
             testcase();
         }
         out.flush();
@@ -115,9 +164,61 @@ public class Main {
         System.err.println("\nRun Time: " + (end - start) / 1000.0 + "s");
     }
 
+    static void dbg(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+    }
+
     /*
-         /\_/\
-        (= ._.)
-        / >  \>
+     * /\_/\
+     * (= ._.)
+     * / > \>
      */
 }
